61ae63f0bf101cacf51692dd66c02a4abc486521
AnastasiaLd <32487197+AnastasiaLd@users.noreply.github.com>
2020-01-02 11:55:05 +0000

    changed current academic year column name
    changed current academic year column name form setid to academic_year

--- lib.php
+++ lib.php
@@ -119,7 +119,7 @@ function setup_enrolments($verbose = false, &$user=null) {
             FROM
                 {$dbtable}
             WHERE
-                  setid = '{$currentacademicyear}'";
+                  academic_year = '{$currentacademicyear}'";
     mtrace($sql);
 
 	// Execute query to get entries from external DB
